2881b43d5a92b28919a1a544c33cdea8af755644
Josh Britz <joshua@onepointfour.io>
2024-03-12 22:45:17 +0200

    Environments

--- frontend/src/pages/request-demo/request-demo.page.tsx
+++ frontend/src/pages/request-demo/request-demo.page.tsx
@@ -13,7 +13,7 @@ export function RequestDemoPage(): JSX.Element {
   const [articleData, setArticleData] = useState<{ isLoading: boolean; data?: IArticle[]; error?: string }>({ isLoading: true });
 
   useEffect(() => {
-    request<IArticle[]>(`${process.env.API_URL}contact/articles`).then(articles => {
+    request<IArticle[]>(`${process.env.REACT_APP_API_URL}contact/articles`).then(articles => {
       if (articles instanceof IErrorResponse || !articles) {
         setArticleData({ isLoading: false, error: `Couldn't load articles` });
       } else setArticleData({ isLoading: false, data: articles });
@@ -23,7 +23,7 @@ export function RequestDemoPage(): JSX.Element {
   async function requestDemo(body: IContactForm) {
     setSubmissionStatus({ isSubmitting: true, isSubmitted: false });
 
-    const response = await request<IContactForm>(`${process.env.API_URL}contact`, { method: 'POST', body });
+    const response = await request<IContactForm>(`${process.env.REACT_APP_API_URL}contact`, { method: 'POST', body });
 
     if (response instanceof IErrorResponse || !response) {
       setSubmissionStatus({ isSubmitting: false, isSubmitted: false });
@@ -273,6 +273,4 @@ export function RequestDemoPage(): JSX.Element {
     </div>
   );
 }
-
-
 
